6b32f6f3001cc8e26f882ce464742e7ec491522d
gmajkic <gmajkic@vente-privee.com>
2020-09-02 20:23:40 +0200

    feat(controller): define optional parameters and his validators

--- validator/company.js
+++ validator/company.js
@@ -1,7 +1,9 @@
 const yup = require('yup');
 
 const companyDetails = yup.object().shape({
-  query: yup.string().trim().required(),
+  companyName: yup.string().trim().required(),
+  postcode: yup.string().length(5),
+  siren: yup.string().length(9),
 });
 
 module.exports = { companyDetails };
